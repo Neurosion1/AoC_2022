31ea8b7ac33d03a5fa32b9ec27c564d2301f5e98
Neurosion1 <neurosion@yahoo.com>
2022-12-01 07:25:38 -0500

    * Day 1

--- AoC_1/AoC_1.cpp
+++ AoC_1/AoC_1.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <numeric>
+#include <set>
 
 int main(int argc, const char * argv[]) {
   std::ifstream input("AoC_1_input.dat");
@@ -14,6 +16,28 @@ int main(int argc, const char * argv[]) {
     std::cout << "Didn't find the input file\n";
     return 1;
   }
+  
+  std::set<long long> all_sums;
+  int sum = 0;
+  while (!input.eof()) {
+    char buf[100] = { 0 };
+    input.getline(buf, 100);
+    if (strlen(buf) > 0) {
+      sum += atoi(buf);
+    }
+    if (strlen(buf) == 0 || input.eof()) {
+      all_sums.insert(sum);
+      sum = 0;
+    }
+  }
+  
+  auto iter = all_sums.rbegin();
+  long long total = *iter++;
+  std::cout << "Part One: " << total << '\n';
+  
+  total += *iter++;
+  total += *iter;
+  std::cout << "Part Two: " << total << '\n';
   
   return 0;
 }
---
+++ AoC_2/AoC_2.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AoC_2
+//
+//  Created by Neurosion on 12/1/22.
+//
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <set>
+
+int main(int argc, const char * argv[]) {
+  std::ifstream input("AoC_2_input.dat");
+  if (input.fail()) {
+    std::cout << "Didn't find the input file\n";
+    return 1;
+  }
+}
