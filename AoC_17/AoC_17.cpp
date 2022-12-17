3262d4782adfee135b4576bf98ed881b5dc08f9f
Neurosion1 <neurosion@yahoo.com>
2022-12-17 10:52:23 -0500

    * Part One Complete

--- AoC_17/AoC_17.cpp
+++ AoC_17/AoC_17.cpp
@@ -18,54 +18,80 @@ namespace
     int x_;
   };
 
-// ####
-//
-// .#.
-// ###
-// .#.
-//
-// ..#
-// ..#
-// ###
-//
-// #
-// #
-// #
-// #
-//
-// ##
-// ##
-
-  typedef std::vector<std::vector<Point2D>> Shape;
+  typedef std::vector<std::vector<int>> Shape;
 
   std::array<Shape, 5> shapes =
   {
     {
       {
-        { { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 } }
+        { 0, 1, 2, 3 }
       },
       {
-        {           { 0, 1 } },
-        { { 1, 0 }, { 1, 1 }, { 1, 2 } },
-        {           { 2, 1 } }
+        {    1    },
+        { 0, 1, 2 },
+        {    1    }
       },
       {
-        {                     { 0, 2 } },
-        {                     { 1, 2 } },
-        { { 2, 0 }, { 2, 1 }, { 2, 2 } }
+        {       2 },
+        {       2 },
+        { 0, 1, 2 }
       },
       {
-        { { 0, 0 } },
-        { { 1, 0 } },
-        { { 2, 0 } },
-        { { 3, 0 } },
+        { 0 },
+        { 0 },
+        { 0 },
+        { 0 },
       },
       {
-        { { 0, 0 }, { 0, 1 } },
-        { { 1, 0 }, { 1, 1 } }
+        { 0, 1 },
+        { 0, 1 }
       }
     }
   };
+  
+  enum Move { eLeft, eRight, eDown };
+  bool try_move(std::deque<std::string>& well, const Shape& shape, int bottom, int y_offset, int x_offset, Move m)
+  {
+    if (m == eDown && y_offset + bottom == 0) {
+      return false;
+    }
+    for (int y = 0; y < shape.size(); ++y) {
+      for (int x = 0; x < shape[y].size(); ++x) {
+        switch (m) {
+          case Move::eDown:
+            if (well[y_offset + bottom + y - 1][x_offset + shape[y][x]] == '#') {
+              return false;
+            }
+            break;
+          case Move::eRight:
+            if ((x_offset + shape[y][x]) == 6 || well[y_offset + y + bottom][x_offset + shape[y][x] + 1] == '#') {
+              return false;
+            }
+            break;
+          case Move::eLeft:
+            if (x_offset == 0 || well[y_offset + y + bottom][x_offset + (shape[y][x] - 1)] == '#') {
+              return false;
+            }
+            break;
+        }
+      }
+    }
+    return true;
+  }
+  
+  void print_well(const std::deque<std::string>& well, const Shape& shape, int bottom, int y_offset, int x_offset)
+  {
+    auto print = well;
+    for (int y = 0; y < shape.size(); ++y) {
+      for (int x = 0; x < shape[y].size(); ++x) {
+        print[bottom + y_offset + y][x_offset + shape[y][x]] = '@';
+      }
+    }
+    for (auto line : print) {
+      std::cout << line << '\n';
+    }
+    std::cout << '\n';
+  }
 }
 
 int main(int argc, const char * argv[]) {
@@ -85,19 +111,47 @@ int main(int argc, const char * argv[]) {
   }
   while (!input.eof());
   
-  std::string well_level = "       ";
-  std::deque<std::string> well;
-  int index = 0, bottom = 0;
-  bool done = false;
-  do {
-    const Shape & this_shape = shapes[index];
-    int x_offset = 2;
-    int y_offset = bottom + 3;
-    if (well.size() < y_offset + this_shape.size()) {
-      
-    }
-  } while (!done);
+  // Flip all of the shapes to "fall up."
+  for (auto & shape : shapes) {
+    std::reverse(shape.begin(), shape.end());
+  }
   
+  std::string well_level = ".......";
+  std::deque<std::string> well;
+  std::vector<std::string> well_chunk(10, well_level);
+  int shape_index = 0, gust_index = 0, bottom = 0, count = 0;
+  do {
+    const Shape & shape = shapes[shape_index++];
+    shape_index %= 5;
+    int x_offset = 2;
+    int y_offset = 3;
+    if (well.size() < bottom + y_offset + shape.size()) {
+      well.insert(well.end(), well_chunk.begin(), well_chunk.end());
+    }
+    bool blocked = false;
+    do {
+      Move move = (gusts[gust_index++] == '>' ? Move::eRight : Move::eLeft);
+      gust_index %= gusts.size();
+      if (try_move(well, shape, bottom, y_offset, x_offset, move)) {
+        x_offset += (move == Move::eRight ? 1 : -1);
+      }
+      if (try_move(well, shape, bottom, y_offset, x_offset, Move::eDown)) {
+        --y_offset;
+      }
+      else {
+        for (int y = 0; y < shape.size(); ++y) {
+          for (int x = 0; x < shape[y].size(); ++x) {
+            well[y_offset + bottom + y][x_offset + shape[y][x]] = '#';
+          }
+        }
+        blocked = true;
+      }
+    } while (!blocked);
+    if (bottom + y_offset + shape.size() > bottom) {
+      bottom += y_offset + shape.size();
+    }
+  } while (++count < 2022);
+  std::cout << "Part One: " << bottom << '\n';
   
   return 0;
 }
